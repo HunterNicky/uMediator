5f32d1468f42ca578e92c3e0b74a6471687a7210
nickyzaguine <nickyzaguine@gmail.com>
2025-07-23 17:42:23 -0300

    umbrella-header

---
+++ include/uMediator/internal/RegisterHandler.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "Mediator.h"
+#define UMEDIATOR_REGISTER_HANDLER(RequestType, HandlerType, InjectionType) \
+namespace umediator::static_dispatch \
+{ \
+    template <> \
+    struct StaticDispatcher<RequestType> \
+    { \
+        static HandlerType& GetHandler(umediator::Mediator& mediator) \
+        { \
+            return mediator.GetInjection<InjectionType>().template create<HandlerType&>(); \
+        } \
+        \
+        static typename RequestType::ResponseType Dispatch(const RequestType& request, umediator::Mediator& mediator) \
+        { \
+            return GetHandler(mediator).Handle(request); \
+        } \
+    }; \
+}
---
+++ include/uMediator/internal/StaticDispatcher.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+namespace umediator
+{
+	class Mediator;
+}
+
+namespace umediator::static_dispatch
+{
+	template <typename>
+	inline constexpr bool kDependentFalse = false;
+
+	template <typename TRequest>
+	struct StaticDispatcher
+	{
+		static typename TRequest::ResponseType Dispatch(const TRequest& request, Mediator& mediator)
+		{
+			static_assert(
+				kDependentFalse<TRequest>,
+				">>> MEDIATOR ERROR: NO HANDLER REGISTERED FOR THIS REQUEST TYPE. <<<\n"
+				"    Possible solutions:\n"
+				"    1. Did you forget to #include the corresponding Handler's header file (.h) where you are calling mediator.Send()?\n"
+				"       Example: #include \"handlers/MyRequestHandler.h\"\n\n"
+				"    2. Did you forget to use the REGISTER_HANDLER(RequestType, HandlerType, ...) macro in the handler's header file?\n"
+			);
+			throw std::runtime_error("Mediator: No registered handler for the request type.");
+		}
+	};
+}
